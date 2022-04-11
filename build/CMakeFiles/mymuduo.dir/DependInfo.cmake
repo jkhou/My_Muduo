9f2feb931ba76360d793c3e93756ef8549fa03d4
jkhou <cquhjk@126.com>
2022-04-11 14:59:50 +0800

    Add My_Muduo code

---
+++ build/CMakeFiles/mymuduo.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hjk/Desktop/mymuduo/Acceptor.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/Acceptor.o"
+  "/home/hjk/Desktop/mymuduo/Buffer.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/Buffer.o"
+  "/home/hjk/Desktop/mymuduo/Channel.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/Channel.o"
+  "/home/hjk/Desktop/mymuduo/CurrentThread.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/CurrentThread.o"
+  "/home/hjk/Desktop/mymuduo/DefaultPoller.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/DefaultPoller.o"
+  "/home/hjk/Desktop/mymuduo/EPollPoller.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/EPollPoller.o"
+  "/home/hjk/Desktop/mymuduo/EventLoop.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/EventLoop.o"
+  "/home/hjk/Desktop/mymuduo/EventLoopThread.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/EventLoopThread.o"
+  "/home/hjk/Desktop/mymuduo/EventLoopThreadPool.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/EventLoopThreadPool.o"
+  "/home/hjk/Desktop/mymuduo/InetAddress.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/InetAddress.o"
+  "/home/hjk/Desktop/mymuduo/Logger.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/Logger.o"
+  "/home/hjk/Desktop/mymuduo/Poller.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/Poller.o"
+  "/home/hjk/Desktop/mymuduo/Socket.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/Socket.o"
+  "/home/hjk/Desktop/mymuduo/TcpConnection.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/TcpConnection.o"
+  "/home/hjk/Desktop/mymuduo/TcpServer.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/TcpServer.o"
+  "/home/hjk/Desktop/mymuduo/Thread.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/Thread.o"
+  "/home/hjk/Desktop/mymuduo/Timestamp.cc" "/home/hjk/Desktop/mymuduo/build/CMakeFiles/mymuduo.dir/Timestamp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/mymuduo.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hjk/Desktop/mymuduo/Acceptor.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/Acceptor.o"
+  "/home/hjk/Desktop/mymuduo/Buffer.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/Buffer.o"
+  "/home/hjk/Desktop/mymuduo/Channel.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/Channel.o"
+  "/home/hjk/Desktop/mymuduo/CurrentThread.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/CurrentThread.o"
+  "/home/hjk/Desktop/mymuduo/DefaultPoller.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/DefaultPoller.o"
+  "/home/hjk/Desktop/mymuduo/EPollPoller.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/EPollPoller.o"
+  "/home/hjk/Desktop/mymuduo/EventLoop.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/EventLoop.o"
+  "/home/hjk/Desktop/mymuduo/EventLoopThread.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/EventLoopThread.o"
+  "/home/hjk/Desktop/mymuduo/EventLoopThreadPool.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/EventLoopThreadPool.o"
+  "/home/hjk/Desktop/mymuduo/InetAddress.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/InetAddress.o"
+  "/home/hjk/Desktop/mymuduo/Logger.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/Logger.o"
+  "/home/hjk/Desktop/mymuduo/Poller.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/Poller.o"
+  "/home/hjk/Desktop/mymuduo/Socket.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/Socket.o"
+  "/home/hjk/Desktop/mymuduo/TcpConnection.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/TcpConnection.o"
+  "/home/hjk/Desktop/mymuduo/TcpServer.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/TcpServer.o"
+  "/home/hjk/Desktop/mymuduo/Thread.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/Thread.o"
+  "/home/hjk/Desktop/mymuduo/Timestamp.cc" "/home/hjk/Desktop/mymuduo/cmake-build-debug/CMakeFiles/mymuduo.dir/Timestamp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "mymuduo_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
